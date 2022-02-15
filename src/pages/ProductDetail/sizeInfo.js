0fcc72be30784c3ebc4f8c423ee3ea04a7c11220
pkw-May <pkw.may@gmail.com>
2022-02-15 19:23:12 +0900

    Add : 관심상품 버튼 개별화작업 및 -상세페이지 모달 버튼 관리 -관심상품 기능 구현 -marketPriceModal 기본레이아웃 구현 -Sider 해결 -불필요한 파일 삭제 -Fixed: 수정사항 반영 -Fixed: 수정사항 2차반영

---
+++ src/pages/ProductDetail/sizeInfo.js
@@ -0,0 +1,16 @@
+const SIZE_INFO = [
+  {
+    id: 1,
+    name: 'Small',
+  },
+  {
+    id: 2,
+    name: 'Medium',
+  },
+  {
+    id: 3,
+    name: 'Large',
+  },
+];
+
+export default SIZE_INFO;

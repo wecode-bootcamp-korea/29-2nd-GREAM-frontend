cd859540dd131f876bb9c474c4e3e5c18252b9cf
pkw-May <pkw.may@gmail.com>
2022-02-15 19:23:12 +0900

    Add: Login Component  - Add: Login layout 생성  - Add: 카카오 계정 직접 입력창추가  - Add: kakao API 추가  - Modify: apply common style  - Fixed: changed KAKAO Redirect URI https -> http  - Modify: changed default modalState true -> false  - Modify: fix z-index value of Login page  - Add: add .env file to gitignore  - Modify: apply styled component  - Modify: cache clear

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Login from './Login/Login';
 
 const Nav = () => {
-  return <div>상단입니다.</div>;
+  const [modalState, setModalState] = useState(false);
+  const closeModal = () => setModalState(false);
+
+  return (
+    <div onKeyUp={e => e.key === 'Escape' && setModalState(false)}>
+      <Login modalState={modalState} closeModal={closeModal} />
+      <button onClick={() => setModalState(true)}>로그인</button>
+      <div>상단입니다.</div>
+    </div>
+  );
 };
 
 export default Nav;

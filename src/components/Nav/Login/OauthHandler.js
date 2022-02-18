41ef205802c5e5e285789061f57710b9b8b7b825
pkw-May <pkw.may@gmail.com>
2022-02-18 19:15:55 +0900

    Add: Post Kako authorize code to server  - Modify: changed modalState name & closeModal name followed by Nav bar  - Modify: separate API URL as config file

--- src/components/Nav/Login/OauthHandler.js
+++ src/components/Nav/Login/OauthHandler.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { flexAlignCenter } from '../../../styles/mixin';
+import { LOGIN_API_URL } from '../../../config';
 
 const OAuthHandler = () => {
-  return <div>로그인 진행중</div>;
+  const location = useLocation();
+
+  useEffect(() => {
+    const authorizeCode = location.search.slice(6);
+    fetch(`${LOGIN_API_URL}/users/login?code=${authorizeCode}`)
+      .then(res => res.json())
+      .then(res => {
+        window.sessionStorage.setItem('JWT', res.access_token);
+      });
+  }, [location]);
+
+  return (
+    <OAth>
+      <Content>
+        <img alt="logo_character" src="/images/logo_character.jpg" />
+        <div> 소셜 로그인 진행 중 :) </div>
+      </Content>
+    </OAth>
+  );
 };
+
+const OAth = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: white;
+  z-index: 1000;
+`;
+
+const Content = styled.div`
+  ${flexAlignCenter}
+  flex-direction: column;
+  top: 50%;
+  right: 50%;
+  bottom: 50%;
+  left: 50%;
+  font-size: 5vh;
+
+  img {
+    width: 30%;
+    margin-top: 30vh;
+    margin-bottom: 5vh;
+  }
+`;
 
 export default OAuthHandler;
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const LOGIN_API_URL = 'http://13.124.44.115:8080';

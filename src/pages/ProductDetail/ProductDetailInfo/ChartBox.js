4a9200c3c4b0ad49166efc55d434c8564d79edc1
박혜린 <likehyelin@gmail.com>
2022-02-23 22:43:35 +0900

    시세 차트 컨테이너 레이아웃

---
+++ src/pages/ProductDetail/ProductDetailInfo/ChartBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import ChartBtn from './ChartBtn';
+
+const ChartBox = () => {
+  return (
+    <div>
+      시세
+      <ChartBtn />
+    </div>
+  );
+};
+
+export default ChartBox;

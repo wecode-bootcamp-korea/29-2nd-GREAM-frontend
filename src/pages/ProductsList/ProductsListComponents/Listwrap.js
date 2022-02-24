087a3267d9433c73a79aaeb9eb0347f9f7bfb3ad
jihongzzang <jujihong2@gmail.com>
2022-02-24 12:22:32 +0900

    Fixed: 수정사항 반영

--- src/pages/ProductsList/ProductsListComponents/Listwrap.js
+++ src/pages/ProductsList/ProductsListComponents/Listwrap.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import LeftFilter from './LeftFilter';
 import RightFilter from './RightFilter';
 
-const Listwrap = ({ FILTER_LISTS, setFilter, list, SORT_LISTS }) => {
+const Listwrap = ({ filterLists, setFilter, list, sortLists }) => {
   return (
     <ListContainer>
-      <LeftFilter FILTER_LISTS={FILTER_LISTS} setFilter={setFilter} />
-      <RightFilter list={list} SORT_LISTS={SORT_LISTS} setFilter={setFilter} />
+      <LeftFilter filterLists={filterLists} setFilter={setFilter} />
+      <RightFilter list={list} sortLists={sortLists} setFilter={setFilter} />
     </ListContainer>
   );
 };
